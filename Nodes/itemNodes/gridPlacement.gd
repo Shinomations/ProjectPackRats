e77647ed05731e8ee10ef53f6aaab25bc74e23be
Shinomations <aaronburch23@gmail.com>
2026-05-30 12:45:16 -0500

    visibility change for placementZone
    only when looking at stuff

--- Nodes/itemNodes/gridPlacement.gd
+++ Nodes/itemNodes/gridPlacement.gd
@@ -7,12 +7,13 @@ const grid: float = 1.0
 func _ready() -> void:
 	await get_tree().process_frame
 	player = get_tree().get_first_node_in_group("player")
-	
+	self.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	if player && player.rayCast.is_colliding():
+		self.visible = true
 		var hit = player.rayCast.get_collision_point()
 		var normal = player.rayCast.get_collision_normal()
 		
@@ -20,4 +21,5 @@ func _process(_delta: float) -> void:
 		var newP = hit + (normal * offset)
 		
 		self.global_position = newP.snapped(Vector3(grid,grid,grid))
-		
+	else:
+		self.visible = false
