04d0e4970d9e771ffe44b3e0272724a00c74e83a
Shinomations <aaronburch23@gmail.com>
2026-05-29 22:33:45 -0500

    gridSystemV3
    this is a new update branch revolving around a new grid system, removal of old 2 as well

---
+++ Nodes/itemNodes/gridPlacement.gd
@@ -0,0 +1,23 @@
+extends StaticBody3D
+
+
+var player
+const grid: float = 1.0
+
+func _ready() -> void:
+	await get_tree().process_frame
+	player = get_tree().get_first_node_in_group("player")
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if player && player.rayCast.is_colliding():
+		var hit = player.rayCast.get_collision_point()
+		var normal = player.rayCast.get_collision_normal()
+		
+		var offset = 0.5
+		var newP = hit + (normal * offset)
+		
+		self.global_position = newP.snapped(Vector3(grid,grid,grid))
+		
